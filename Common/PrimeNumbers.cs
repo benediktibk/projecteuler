4b5fa9e6b2e9e7735afd5e849faefd48ab93f5fa
Benedikt Schmidt <benediktibk@aon.at>
2015-01-19 20:13:51 +0100

    implemented PrimeNumbers

---
+++ Common/PrimeNumbers.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Common
+{
+    public class PrimeNumbers
+    {
+        private readonly List<int> _primeNumbers;
+        private int _currentUpperBorder;
+
+        public PrimeNumbers()
+        {
+            _primeNumbers = new List<int>();
+            _currentUpperBorder = 1;
+        }
+
+        public List<int> UpTo(int limit)
+        {
+            ExtendPrimeNumbers(limit);
+            return UpToInternal(limit);
+        }
+
+        private List<int> UpToInternal(int limit)
+        {
+            if (limit > _currentUpperBorder)
+                throw new ArgumentException("limit");
+
+            var lower = 0;
+            var upper = _primeNumbers.Count - 1;
+
+            if (_primeNumbers[upper] == limit)
+                return new List<int>(_primeNumbers);
+
+            while (upper - lower > 1)
+            {
+                var middle = (upper + lower)/2;
+
+                if (_primeNumbers[middle] < limit)
+                    lower = middle;
+                else
+                    upper = middle;
+            }
+
+            return _primeNumbers.GetRange(0, upper + 1);
+        }
+
+        private void ExtendPrimeNumbers(int limit)
+        {
+            if (limit <= _currentUpperBorder)
+                return;
+
+            var offset = _currentUpperBorder + 1;
+            var notPrime = new bool[limit - _currentUpperBorder];
+
+            foreach (var primeNumber in _primeNumbers)
+            {
+                var nextCandidate = (_currentUpperBorder/primeNumber + 1)*primeNumber;
+
+                while (nextCandidate <= limit)
+                {
+                    notPrime[nextCandidate - offset] = true;
+                    nextCandidate += primeNumber;
+                }
+            }
+
+            var newPrimeNumbers = new LinkedList<int>();
+
+            for (var candidate = _currentUpperBorder + 1; candidate <= limit; ++candidate)
+            {
+                if (notPrime[candidate - offset])
+                    continue;
+
+                var nextCandidate = candidate * 2; 
+                
+                while (nextCandidate <= limit)
+                {
+                    notPrime[nextCandidate - offset] = true;
+                    nextCandidate += candidate;
+                }
+
+                newPrimeNumbers.AddLast(candidate);
+            }
+
+            _primeNumbers.AddRange(newPrimeNumbers);
+
+            _currentUpperBorder = limit;
+        }
+    }
+}
---
+++ CommonTest/PrimeNumbersTest.cs
@@ -0,0 +1,75 @@
+﻿using Common;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CommonTest
+{
+    [TestClass]
+    public class PrimeNumbersTest
+    {
+        [TestMethod]
+        public void UpTo_5_CorrectResult()
+        {
+            var primeNumbers = new PrimeNumbers();
+
+            var result = primeNumbers.UpTo(5);
+
+            Assert.AreEqual(3, result.Count);
+            Assert.AreEqual(2, result[0]);
+            Assert.AreEqual(3, result[1]);
+            Assert.AreEqual(5, result[2]);
+        }
+
+        [TestMethod]
+        public void UpTo_25_CorrectResult()
+        {
+            var primeNumbers = new PrimeNumbers();
+
+            var result = primeNumbers.UpTo(25);
+
+            Assert.AreEqual(9, result.Count);
+            Assert.AreEqual(2, result[0]);
+            Assert.AreEqual(3, result[1]);
+            Assert.AreEqual(5, result[2]);
+            Assert.AreEqual(7, result[3]);
+            Assert.AreEqual(11, result[4]);
+            Assert.AreEqual(13, result[5]);
+            Assert.AreEqual(17, result[6]);
+            Assert.AreEqual(19, result[7]);
+            Assert.AreEqual(23, result[8]);
+        }
+
+        [TestMethod]
+        public void UpTo_25And5_CorrectResult()
+        {
+            var primeNumbers = new PrimeNumbers();
+
+            primeNumbers.UpTo(25);
+            var result = primeNumbers.UpTo(5);
+
+            Assert.AreEqual(3, result.Count);
+            Assert.AreEqual(2, result[0]);
+            Assert.AreEqual(3, result[1]);
+            Assert.AreEqual(5, result[2]);
+        }
+
+        [TestMethod]
+        public void UpTo_5And25_CorrectResult()
+        {
+            var primeNumbers = new PrimeNumbers();
+
+            primeNumbers.UpTo(5);
+            var result = primeNumbers.UpTo(25);
+
+            Assert.AreEqual(9, result.Count);
+            Assert.AreEqual(2, result[0]);
+            Assert.AreEqual(3, result[1]);
+            Assert.AreEqual(5, result[2]);
+            Assert.AreEqual(7, result[3]);
+            Assert.AreEqual(11, result[4]);
+            Assert.AreEqual(13, result[5]);
+            Assert.AreEqual(17, result[6]);
+            Assert.AreEqual(19, result[7]);
+            Assert.AreEqual(23, result[8]);
+        }
+    }
+}
