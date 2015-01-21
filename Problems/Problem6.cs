afabd5c7efd25736b0f1155e9b5761a2f52cae40
Benedikt Schmidt <benediktibk@aon.at>
2015-01-21 22:54:13 +0100

    solved problem 6

---
+++ Problems/Problem6.cs
@@ -0,0 +1,25 @@
+﻿using Common;
+
+namespace Problems
+{
+    public class Problem6 : IProblem
+    {
+        private readonly int _upperBorder;
+
+        public Problem6(int upperBorder)
+        {
+            _upperBorder = upperBorder;
+        }
+
+        public long Solve()
+        {
+            var sum = (_upperBorder + 1)*_upperBorder/2;
+            var sumOfSquares = 0;
+
+            for (var i = 1; i <= _upperBorder; ++i)
+                sumOfSquares += i*i;
+
+            return sum * sum - sumOfSquares;
+        }
+    }
+}
---
+++ ProblemsTest/Problem6Test.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Problems;
+
+namespace ProblemsTest
+{
+    [TestClass]
+    public class Problem6Test
+    {
+        [TestMethod]
+        public void Solve_UpperBorder10_2640()
+        {
+            var problem = new Problem6(10);
+
+            Assert.AreEqual(2640, problem.Solve());
+        }
+
+        [TestMethod]
+        public void Solve_UpperBorder100_25164150()
+        {
+            var problem = new Problem6(100);
+
+            Assert.AreEqual(25164150, problem.Solve());
+        }
+    }
+}
