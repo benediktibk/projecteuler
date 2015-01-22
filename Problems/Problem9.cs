a3d674b948635ffabfd6f90f3b124c69af222927
Benedikt Schmidt <benediktibk@aon.at>
2015-01-22 08:55:32 +0100

    improved solution for problem 9

--- Problems/Problem9.cs
+++ Problems/Problem9.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using Common;
 
 namespace Problems
@@ -11,15 +12,13 @@ namespace Problems
         public long Solve()
         {
             for (var c = TargetSum - 1; c > 0; --c)
-                for (var b = c - 1; b > 0; --b)
-                    for (var a = b - 1; a > 0; --a)
-                    {
-                        if (a*a + b*b != c*c)
-                            continue;
+                for (var b = 1; b < TargetSum - c; ++b)
+                {
+                    var a = TargetSum - b - c;
 
-                        if (a + b + c == TargetSum)
-                            return a*b*c;
-                    }
+                    if (a * a + b * b == c * c)
+                        return a * b * c;
+                }
 
             return 0;
         }
