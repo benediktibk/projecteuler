e86c893d99874699119d996c9a1a31ef12459843
Benedikt Schmidt <benediktibk@aon.at>
2015-01-24 18:21:31 +0100

    solved problem 16

---
+++ Problems/Problem16.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using Common;
+
+namespace Problems
+{
+    public class Problem16 : IProblem
+    {
+        private readonly int _exponent;
+
+        public Problem16(int exponent)
+        {
+            if (exponent < 0)
+                throw new ArgumentException("exponent");
+
+            _exponent = exponent;
+        }
+
+        public long Solve()
+        {
+            var number = new List<int> {1};
+
+            for (var i = 0; i < _exponent; ++i)
+            {
+                var carry = 0;
+
+                for (var j = 0; j < number.Count; ++j)
+                {
+                    var product = number[j]*2 + carry;
+                    var digit = product - product/10*10;
+                    carry = (product - digit)/10;
+                    number[j] = digit;
+                }
+
+                while (carry > 0)
+                {
+                    var digit = carry - carry/10*10;
+                    carry = carry/10;
+                    number.Add(digit);
+                }
+            }
+
+            return number.Sum();
+        }
+    }
+}
---
+++ ProblemsTest/Problem16Test.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Problems;
+
+namespace ProblemsTest
+{
+    [TestClass]
+    public class Problem16Test
+    {
+        [TestMethod]
+        public void Solve_Exponent15_26()
+        {
+            var problem = new Problem16(15);
+
+            Assert.AreEqual(26, problem.Solve());
+        }
+
+        [TestMethod]
+        public void Solve_Exponent1000_1366()
+        {
+            var problem = new Problem16(1000);
+
+            Assert.AreEqual(1366, problem.Solve());
+        }
+    }
+}
