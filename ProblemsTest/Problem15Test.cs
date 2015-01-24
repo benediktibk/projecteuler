7904c3a85a5d43f18bc38372ce5c265387abbee1
Benedikt Schmidt <benediktibk@aon.at>
2015-01-24 19:34:56 +0100

    added tests for problem 15

--- ProblemsTest/Problem15Test.cs
+++ ProblemsTest/Problem15Test.cs
@@ -13,5 +13,37 @@ namespace ProblemsTest
 
             Assert.AreEqual(6, problem.Solve());
         }
+
+        [TestMethod]
+        public void Solve_Dimension4_70()
+        {
+            var problem = new Problem15(4);
+
+            Assert.AreEqual(70, problem.Solve());
+        }
+
+        [TestMethod]
+        public void Solve_Dimension6_924()
+        {
+            var problem = new Problem15(6);
+
+            Assert.AreEqual(924, problem.Solve());
+        }
+
+        [TestMethod]
+        public void Solve_Dimension8_12870()
+        {
+            var problem = new Problem15(8);
+
+            Assert.AreEqual(12870, problem.Solve());
+        }
+
+        [TestMethod]
+        public void Solve_Dimension10_184756()
+        {
+            var problem = new Problem15(10);
+
+            Assert.AreEqual(184756, problem.Solve());
+        }
     }
 }
