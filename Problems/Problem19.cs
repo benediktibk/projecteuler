960023149feb838f94f22e809f77a55df7e250d0
Benedikt Schmidt <benediktibk@aon.at>
2015-01-25 11:06:03 +0100

    solved problem 19

---
+++ Problems/Problem19.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Common;
+
+namespace Problems
+{
+    public class Problem19 : IProblem
+    {
+        public long Solve()
+        {
+            var currentDate = new DateTime(1901, 1, 1);
+            var endDate = new DateTime(2000, 12, 31);
+
+            while (currentDate.DayOfWeek != DayOfWeek.Sunday)
+                currentDate = currentDate.AddDays(1);
+
+            var result = 0;
+
+            while (currentDate < endDate)
+            {
+                if (currentDate.Date.Day == 1)
+                    result++;
+
+                currentDate = currentDate.AddDays(7);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ ProblemsTest/Problem19Test.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Problems;
+
+namespace ProblemsTest
+{
+    [TestClass]
+    public class Problem19Test
+    {
+        [TestMethod]
+        public void Solve_Empty_171()
+        {
+            var problem = new Problem19();
+
+            Assert.AreEqual(171, problem.Solve());
+        }
+    }
+}
