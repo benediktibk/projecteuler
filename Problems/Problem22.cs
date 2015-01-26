25afb931eaffd934dcbb747c6d67c46555d3f0db
Benedikt Schmidt <benediktibk@aon.at>
2015-01-26 19:28:13 +0100

    solved problem 22

---
+++ Problems/Problem22.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using Common;
+
+namespace Problems
+{
+    public class Problem22 : IProblem
+    {
+        public long Solve()
+        {
+            var content = GetContent();
+            var contentSplit = content.Split(',');
+            var names = new List<string>(contentSplit.Count());
+
+            foreach (var element in contentSplit)
+            {
+                var name = element.Substring(1, element.Count() - 2);
+                names.Add(name);
+            }
+
+            names.Sort();
+            var valueOfA = Convert.ToInt64('A');
+            long result = 0;
+
+            for (var i = 0; i < names.Count; i++)
+            {
+                var value = names[i].Sum(x => Convert.ToInt64(x) - valueOfA + 1);
+                long weight = i + 1;
+                result += value*weight;
+            }
+
+            return result;
+        }
+
+        public static string GetContent()
+        {
+            var assembly = Assembly.GetExecutingAssembly(); ;
+            var stream = assembly.GetManifestResourceStream("Problems.p022_names.txt");
+            var streamReader = new StreamReader(stream);
+            return streamReader.ReadToEnd();
+        }
+    }
+}
---
+++ ProblemsTest/Problem22Test.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Problems;
+
+namespace ProblemsTest
+{
+    [TestClass]
+    public class Problem22Test
+    {
+        [TestMethod]
+        public void Solve_Empty_CorrectResult()
+        {
+            var problem = new Problem22();
+
+            Assert.AreEqual(-1, problem.Solve());
+        }
+    }
+}
