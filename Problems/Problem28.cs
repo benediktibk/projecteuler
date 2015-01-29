7b65725d000b95fbde8fcff6ec8c7c39fe80ef15
Benedikt Schmidt <benediktibk@aon.at>
2015-01-29 14:05:14 +0100

    prepared problem 28

---
+++ Problems/Problem28.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Common;
+
+namespace Problems
+{
+    public class Problem28 : IProblem
+    {
+        private readonly int _dimension;
+
+        public Problem28(int dimension)
+        {
+            if (dimension < 1)
+                throw new ArgumentOutOfRangeException("dimension");
+
+            if (dimension%2 == 0)
+                throw new ArgumentOutOfRangeException("dimension");
+
+            _dimension = dimension;
+        }
+
+        public long Solve()
+        {
+            var result = 1;
+            var current = 1;
+
+            for (var i = 3; i <= _dimension; i += 2)
+            {
+                for (var j = 0; j < 4; ++j)
+                {
+                    current += i - 1;
+                    result += current;
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ ProblemsTest/Problem28Test.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Problems;
+
+namespace ProblemsTest
+{
+    [TestClass]
+    public class Problem28Test
+    {
+        [TestMethod]
+        public void Solve_Dimension1_1()
+        {
+            var problem = new Problem28(1);
+
+            Assert.AreEqual(1, problem.Solve());
+        }
+
+        [TestMethod]
+        public void Solve_Dimension3_25()
+        {
+            var problem = new Problem28(3);
+
+            Assert.AreEqual(25, problem.Solve());
+        }
+
+        [TestMethod]
+        public void Solve_Dimension5_101()
+        {
+            var problem = new Problem28(5);
+
+            Assert.AreEqual(101, problem.Solve());
+        }
+
+        [TestMethod]
+        public void Solve_Dimension1001_0()
+        {
+            var problem = new Problem28(1001);
+
+            Assert.AreEqual(0, problem.Solve());
+        }
+    }
+}
